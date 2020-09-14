327c9f592ccdd4046b187307caf64ffdae26d8e9
PaniPaniello <62522202+PaniPaniello@users.noreply.github.com>
2020-09-14 20:57:44 +0200

    Add files via upload

---
+++ Merge_panoistic.sh
@@ -0,0 +1,2 @@
+
+cat /home/therea/Desktop/TFMOscar/6.Collapse_Replicates/6.2.Collapsed_Merged_Libraries/Blattella_germanica.fasta.collapsed.fa /home/therea/Desktop/TFMOscar/6.Collapse_Replicates/6.2.Collapsed_Merged_Libraries/Locusta_migratoria.fasta.collapsed.fa > /home/therea/Desktop/TFMOscar/8.Ovary_Libraries/Panoistic_piRNA.fasta
---
+++ Merge_telotrophic.sh
@@ -0,0 +1,3 @@
+
+cat /home/therea/Desktop/TFMOscar/6.Collapse_Replicates/6.2.Collapsed_Merged_Libraries/Oncopeltus_fasciatus.fasta.collapsed.fa /home/therea/Desktop/TFMOscar/6.Collapse_Replicates/6.2.Collapsed_Merged_Libraries/Acyrtosiphon_pisum.fasta.collapsed.fa /home/therea/Desktop/TFMOscar/6.Collapse_Replicates/6.2.Collapsed_Merged_Libraries/Tribolium_castaneum.fasta.collapsed.fa /home/therea/Desktop/TFMOscar/6.Collapse_Replicates/6.2.Collapsed_Merged_Libraries/Diabrotica_virgifera.fasta.collapsed.fa /home/therea/Desktop/TFMOscar/6.Collapse_Replicates/6.2.Collapsed_Merged_Libraries/Nicrophorus_vespilloides.fasta.collapsed.fa  > /home/therea/Desktop/TFMOscar/8.Ovary_Libraries/M_Telotrophic_piRNA.fasta
+
